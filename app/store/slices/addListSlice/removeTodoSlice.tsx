a623f779ec70f3bf60e91a142286fb8c134a400d
Muhammad Kashif <gmdcf797@gmail.com>
2024-04-24 10:11:38 +0500

    removeList  extraReducers and action create

--- app/store/slices/addListSlice/removeTodoSlice.tsx
+++ app/store/slices/addListSlice/removeTodoSlice.tsx
@@ -1,5 +1,6 @@
 import { RemoveListState } from "@/types";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState: RemoveListState = {
   data: null,
@@ -7,15 +8,35 @@ const initialState: RemoveListState = {
   error: null,
 };
 
-export const RemoveList = createAsyncThunk("remove", async () => {
+export const RemoveList = createAsyncThunk("remove", async (id: string) => {
   try {
-  } catch (error) {}
+    const confirmed = confirm("Are you sure you want to remove?");
+    if (confirmed) {
+      await axios.delete(`${URL}/api/list/?id=${id}`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
-export const counterSlice = createSlice({
-  name: "counter",
+export const removeSlice = createSlice({
+  name: "removeList",
 
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(RemoveList.pending, (state) => {
+        state.loading = false;
+      })
+      .addCase(RemoveList.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(RemoveList.rejected, (state, action) => {
+        state.loading = false;
+        state.error = null;
+      });
+  },
 });
 
-export default counterSlice.reducer;
+export default removeSlice.reducer;
